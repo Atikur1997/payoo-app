7f02138d229f80e71ba6ad8fdae9d3c1888986b4
Atikur Rahman <nishanrhman@gmail.com>
2025-08-25 22:39:54 +0600

    cashout section done with validation

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+const cashOutParent = document.getElementById('cashoutParent');
+
 document.getElementById('addMoney').addEventListener('click', (e) => {
     e.preventDefault();
     const bankName = document.getElementById('bankName').value;
@@ -25,3 +27,43 @@ document.getElementById('addMoney').addEventListener('click', (e) => {
     document.getElementById('balance').innerText = newAmount;
 
 })
+
+// toggling features
+
+document.getElementById('addMoneyBtn').addEventListener('click', () => {
+    cashOutParent.classList.add('hidden');
+    document.getElementById('add-money-parent').classList.remove('hidden');
+    document.getElementById('add-money-parent').classList.add('block');
+})
+//Cash out section
+document.getElementById('cashOut').addEventListener('click', () => {
+    cashOutParent.classList.remove('hidden');
+    cashOutParent.classList.add('block');
+    document.getElementById('add-money-parent').classList.add('hidden');
+
+})
+
+document.getElementById("Cashbtn").addEventListener("click", () => {
+
+    const pinNumber = 3344;
+    const pin2 = parseInt(document.getElementById('pinCashOut').value);
+    let mainBalance = parseFloat(document.getElementById('balance').innerText)
+
+
+    if (document.getElementById("accountNumber1").value.length < 11) {
+        alert("Please enter a valid account number");
+        return;
+    }
+    if (pin2 !== pinNumber) {
+        alert('Please enter a valid pin');
+        return;
+    }
+    if (parseFloat(document.getElementById('balance').innerText) < parseFloat(document.getElementById('amountToWithdraw').value)) {
+        alert('Insufficient Balance');
+        return;
+    }
+
+    let newAmount = parseFloat(mainBalance - parseFloat(document.getElementById('amountToWithdraw').value)).toFixed(2);
+    document.getElementById('balance').innerText = newAmount;
+})
+
